85ce0cfb2a9343dc2085c60a2d77b9bce3e6356a
tusarpatel <167279662+tusarpatel@users.noreply.github.com>
2024-05-13 18:05:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 6848223695)
+OWNER_ID = int(getenv("OWNER_ID", None)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/anime_in_hindi_pills")
