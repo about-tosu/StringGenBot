dd307fa72f2dafe8a7f416229c4645a1c77e4381
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-08-15 01:23:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = 24620300
+API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
+BOT_TOKEN = "6865478876:AAHyWDmRcLq48f6ikph4uWcPH0nnkkFXGjw"
+MONGO_DB_URI = "mongodb+srv://botmaker9675208:botmaker9675208@cluster0.sc9mq8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-OWNER_ID = int(getenv("OWNER_ID", 6848223695))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/about_tosuu")
+OWNER_ID = "6848223695"
+SUPPORT_CHAT = "https://t.me/about_tosuu"
